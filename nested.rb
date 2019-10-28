d938ae3e50eddda3476526aef6d8ccafb16390d7
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 14:43:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages].shift "Assembly"
+    programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+    programmer_hash
 end
